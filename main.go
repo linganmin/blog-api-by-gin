34018046b8f85b882efc73ee5358bcb61046ccca
lingan <saboran@163.com>
2019-05-04 22:49:06 +0800

    删除一些没有的方法&增加.log问价忽略

--- main.go
+++ main.go
@@ -1,19 +1,8 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"ginhello/routes"
 )
-import "net/http"
-import "ginhello/routes"
-
-func demo(c *gin.Context) {
-	c.JSON(http.StatusOK,gin.H{
-		"code":0,
-		"msg":"success",
-		"data":make(map[string]string),
-	})
-}
 func main() {
 	routes.Init()
-	//routes.AdminInit()
 }
